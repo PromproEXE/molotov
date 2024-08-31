fdd8802c58cf32e43799e23e82913b1dbed18386
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-08-31 11:22:29 +0700

    DEL: UNUSED CODE

--- apps/server/src/trpc/trpc.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TRPCService } from './trpc.service';
-import { TRPCRouter } from './trpc.router';
-
-@Module({
-  imports: [],
-  providers: [TRPCService, TRPCRouter],
-})
-export class TRPCModule {}
