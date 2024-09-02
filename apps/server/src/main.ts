0cfb326d8ec2c22004326dadf813589168b38033
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-09-03 02:48:46 +0700

    DISABLE CORS

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -3,18 +3,18 @@ import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+    // app.enableCors();
 
-  app.enableVersioning();
+    app.enableVersioning();
 
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-    }),
-  );
+    app.useGlobalPipes(
+        new ValidationPipe({
+            transform: true,
+        }),
+    );
 
-  await app.listen(process.env.PORT || 3333);
+    await app.listen(process.env.PORT || 3333);
 }
 bootstrap();
