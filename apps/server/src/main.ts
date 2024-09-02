75a890e736cf93498069660a64aeab7dc03d1229
promproexe <63090004+PromproEXE@users.noreply.github.com>
2024-09-03 02:55:25 +0700

    FIX: CORS

--- apps/server/src/main.ts
+++ apps/server/src/main.ts
@@ -5,7 +5,11 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
 
-    // app.enableCors();
+    app.enableCors({
+        origin: true,
+        methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+        credentials: true,
+    });
 
     app.enableVersioning();
 
